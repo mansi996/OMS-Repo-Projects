8e8ae5df7873425879621cb0c08b84e4a32fd57e
Mansi Anand <mansianand996@gmail.com>
2020-03-11 15:01:26 +0530

    commit 1

---
+++ src/main/java/com/example/demo/DemoProjectSpring3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectSpring3Application.class, args);
+	}
+
+}
